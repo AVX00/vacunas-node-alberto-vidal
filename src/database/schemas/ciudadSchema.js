75f78730256e93c664f7e660cc8f379c16695f31
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-02-24 16:43:33 +0100

    created ciudadSchema

--- src/database/schemas/ciudadSchema.js
+++ src/database/schemas/ciudadSchema.js
@@ -1,5 +1,11 @@
-const {Schema } = require('mongoose')
+const { Schema } = require("mongoose");
+const puntoVacunacionSchema = require("./puntoVacunacionSchema");
 
-const ciudadSchema = new Schema ({
-  nombre: {type: }
-})
+const ciudadSchema = new Schema({
+  name: { type: String, required: true },
+  puntosVacunacion: {
+    type: [puntoVacunacionSchema],
+  },
+});
+
+module.exports = ciudadSchema;
